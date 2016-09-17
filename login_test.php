6e680c01799ec7afe63a4253034ff948adbf3fe4
milk448249712 <448249712@qq.com>
2016-09-17 12:53:51 +0800

    databese authorization.

--- login_test.php
+++ login_test.php
@@ -1,10 +1,23 @@
 <?php
-        $name = $_POST["name"];
+	$name = $_POST["name"];
 	$pwd = $_POST["pwd"];
-        echo "name:".$name."<br>";   
-        echo "pwd:".$pwd."<br>";
-        if (1) {
-            echo "authorization valid!<br>";
-        }
-        header("refresh:3; URL='chartroom.php'");
+	echo "name:".$name."<br>";   
+	echo "pwd:".$pwd."<br>";
+	$link_ID=mysql_connect("localhost","miracle");// 链接Mysql服务器
+	if ($link_ID) {
+		echo 'connected';
+	}
+	if (mysql_select_db("test")) // 选择数据库
+	{
+		echo 'select db ok';
+	}
+	mysql_query("set names 'gbk'");	// 选择编码格式
+	if (date_default_timezone_set("Asia/Hong_Kong")) // 设置发言时间格式
+	{
+		echo 'set time ok!';
+	}
+	if (1) {
+		echo "authorization valid!<br>";
+	}
+	header("refresh:3; URL='chartroom.php'");
 ?>
