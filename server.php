94c2537227d6bba9c1612e06ffee7b971158e951
milk448249712 <448249712@qq.com>
2016-09-27 19:11:39 +0800

    memory shared

--- server.php
+++ server.php
@@ -1,3 +1,13 @@
 <?php
-    
->
+    $systemid = 1; // System ID for the shared memory segment
+    $mode = "c"; // Access mode
+    $permissions = 0755; // Permissions for the shared memory segment
+    $size = 1024; // Size, in bytes, of the segment
+    $shmid = shmop_open($systemid, $mode, $permissions, $size);
+    $memstr = '';
+    while(1) {
+        sleep(10);
+        $size = shmop_size($shmid);
+        $memstr = $shmop_read($shmid, 0, $size);
+    }
+?>
