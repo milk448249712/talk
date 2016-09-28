a099e416e18417ef41340669e7d66d8c3021617b
milk448249712 <448249712@qq.com>
2016-09-28 17:31:24 +0800

    share memory

--- friends.php
+++ friends.php
@@ -1,6 +1,6 @@
 <?php
-    $xml = simplexml_load_file("friends.xml");
-    echo $xml->getName();
+    $xml = simplexml_load_file("F:\\miracle\\talk\\friends_unnkai.xml");
+    echo 'xml:'.$xml;
     echo "\n";
     $i = 0;
     //$pal = $xml->addChild("pal");
@@ -15,8 +15,19 @@
             $i ++;
         }
     }
-    $pal = $xml->addChild("pal");
-    $pal->addAttribute('id', "$i");
-    $pal->addChild("name","frn$i");
-    $xml->asXML("friends.xml");
+    //$pal = $xml->addChild("pal");
+    //$pal->addAttribute('id', "$i");
+    //$pal->addChild("name","frn$i");
+    //$xml->asXML("friends.xml");
+    $systemid = 1; // System ID for the shared memory segment
+    $mode = "c"; // Access mode
+    $permissions = 0755; // Permissions for the shared memory segment
+    $size = 1024; // Size, in bytes, of the segment
+
+    $shmid = shmop_open($systemid, $mode, $permissions, $size);
+    shmop_write($shmid, "123test", 0);
+    echo 'done.';
+    while(1) {
+        sleep(10);
+    }
 ?>
--- server.php
+++ server.php
@@ -5,9 +5,17 @@
     $size = 1024; // Size, in bytes, of the segment
     $shmid = shmop_open($systemid, $mode, $permissions, $size);
     $memstr = '';
+    // $semid=sem_get(0x2,1,0666);
     while(1) {
         sleep(10);
+        echo "waitting...\n";
+        // sem_acquire($semid);    // acquire the semaphore
         $size = shmop_size($shmid);
-        $memstr = $shmop_read($shmid, 0, $size);
+        $memstr = shmop_read($shmid, 0, $size);
+        echo $memstr;
+        // friends list function
+        // do sth.
     }
+    shmop_delete($shmid);
+    shmop_close($shmid);
 ?>
