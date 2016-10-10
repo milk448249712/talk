3e82ea55e7c3dd36b21d9d933aae2d4da572d403
milk448249712 <448249712@qq.com>
2016-10-10 09:48:58 +0800

    merge with file on linux

--- friends.php
+++ friends.php
@@ -1,8 +1,37 @@
 <?php
-    $xml = simplexml_load_file("F:\\miracle\\talk\\friends_unnkai.xml");
-    echo 'xml:'.$xml;
-    echo "\n";
-    $i = 0;
+    echo "load friends!<br>";
+    $name = $_SESSION['user'];
+    //echo $name;
+    $xml_filename = "friends_".$name.".xml";
+    $xml = simplexml_load_file($xml_filename);
+?>
+<html>
+<table border="1">
+<tr>
+<th>Heading</th>
+</tr>
+<?php
+    foreach($xml->children() as $child) {
+?>
+    <tr>
+    <td>
+    <?php
+    echo $child->attributes()." ";
+    echo "<a href=\"\">";
+    foreach($child->children() as $childv2)
+    {
+        echo "$childv2";
+    }
+    echo "</a>"
+    ?>
+    </td>
+    </tr>
+<?php
+}
+?>
+</table>
+</html>
+<?php
     //$pal = $xml->addChild("pal");
     //$pal->addAttribute('id', "$i");
     //$pal->addChild("name","frn$i");
